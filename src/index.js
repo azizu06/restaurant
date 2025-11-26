45fdf080f1aa9a2b9e4569e8f17069bc4c4f7881
azizu06 <ab725492@ucf.edu>
2025-11-25 20:10:05 -0500

    Add listeners for nav buttons on entry point module

---
+++ src/index.js
@@ -0,0 +1,34 @@
+import "./styles.css";
+import "@mdi/font/css/materialdesignicons.min.css";
+import { home } from "./home.js";
+import { about } from "./about.js";
+import { contact } from "./contact.js";
+import { menu } from "./menu.js";
+
+const main = document.querySelector('.content');
+const homeBtn = document.querySelector(".home");
+const menuBtn = document.querySelector(".menu");
+const aboutBtn = document.querySelector(".about");
+const contactBtn = document.querySelector(".contact");
+
+main.appendChild(home());
+
+homeBtn.addEventListener("click", () => {
+    main.innerHTML = "";
+    main.appendChild(home());
+});
+
+menuBtn.addEventListener("click", () => {
+    main.innerHTML = "";
+    main.appendChild(menu());
+});
+
+aboutBtn.addEventListener("click", () => {
+    main.innerHTML = "";
+    main.appendChild(about());
+});
+
+contactBtn.addEventListener("click", () => {
+    main.innerHTML = "";
+    main.appendChild(contact());
+});
