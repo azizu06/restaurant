8abcfffdc16ca3538c534323468026b81201a8d3
azizu06 <ab725492@ucf.edu>
2025-11-25 19:13:24 -0500

    Update webpack config for mdi svgs

--- webpack.config.js
+++ webpack.config.js
@@ -25,9 +25,13 @@ module.exports = {
             },
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                use: "asset/resource",
-            }
-        ]
+                type: "asset/resource",
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: "asset/resource",
+            },
+        ],
     },
     plugins: [
         new HtmlWebpackPlugin({
